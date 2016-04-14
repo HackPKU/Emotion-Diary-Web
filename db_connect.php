ffca5ee31f73a65734f798c15320d7d515b7b99f
范志康 <goodman.capu@gmail.com>
2016-04-15 01:56:24 +0800

    MySQL Connect

--- db_connect.php
+++ db_connect.php
@@ -4,11 +4,13 @@
  */
 require_once 'config.php';
 
-// Database connector.
-function dbconnect() {
-    $con = @mysql_connect(EMOTION_DIARY_DB_HOSTNAME, EMOTION_DIARY_DB_USERNAME, EMOTION_DIARY_DB_PASSWORD) or die("Cannot connect to database!");
-    mysql_query("SET NAMES 'UTF8'");
-    mysql_select_db(EMOTION_DIARY_DB_NAME);
+/**
+ * Database connector.
+ */
+function db_connect() {
+    $con = mysqli_connect(EMOTION_DIARY_DB_HOSTNAME, EMOTION_DIARY_DB_USERNAME, EMOTION_DIARY_DB_PASSWORD, EMOTION_DIARY_DB_NAME);
+    $con->query("SET NAMES 'UTF8'");
+    return $con;
 }
 
 ?>
---
+++ db_connect_test.php
@@ -0,0 +1,14 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+
+<?php
+
+require_once "db_connect.php";
+$con = db_connect();
+
+if ($con->query("select * from user")) {
+    echo "数据库连接成功";
+}else {
+    echo "数据库链接失败";
+}
+
+?>
