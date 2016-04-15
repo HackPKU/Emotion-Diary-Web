a4f33b73f4edd6f73025f737257e0d9c4b77b3fb
范志康 <goodman.capu@gmail.com>
2016-04-15 13:19:45 +0800

    Database Structure

--- db_connect.php
+++ db_connect.php
@@ -8,7 +8,7 @@ require_once 'config.php';
  * Database connector.
  */
 function db_connect() {
-    $con = mysqli_connect(EMOTION_DIARY_DB_HOSTNAME, EMOTION_DIARY_DB_USERNAME, EMOTION_DIARY_DB_PASSWORD, EMOTION_DIARY_DB_NAME);
+    $con = mysqli_connect(EMOTION_DIARY_DB_HOSTNAME, EMOTION_DIARY_DB_USERNAME, EMOTION_DIARY_DB_PASSWORD, EMOTION_DIARY_DB_NAME) or die("数据库不存在或用户名密码不正确");
     $con->query("SET NAMES 'UTF8'");
     return $con;
 }
--- initialize.sql
+++ initialize.sql
@@ -3,12 +3,37 @@
 -- Table structure for table `user`
 
 CREATE TABLE `user` (
-  `id` varchar(16) NOT NULL,
-  `name` varchar(16) NOT NULL,
-  `sex` varchar(16) NOT NULL,
-  `email` varchar(128) NOT NULL,
-  `icon` varchar(128) NOT NULL,
-  `register_time` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
-  `latest_time` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
-  PRIMARY KEY(`id`)
+  `userid` VARCHAR(16) NOT NULL,
+  `name` VARCHAR(16) NOT NULL,
+  `sex` VARCHAR(16) NOT NULL,
+  `email` VARCHAR(128) NOT NULL,
+  `icon` VARCHAR(128) NOT NULL,
+  `register_time` DATETIME NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `latest_time` DATETIME NOT NULL DEFAULT '1000-01-01 00:00:00',
+  PRIMARY KEY (`userid`)
 );
+
+-- Table structure for table `token`
+
+CREATE TABLE `token` (
+  `token` VARCHAR(128) NOT NULL,
+  `userid` VARCHAR(16) NOT NULL,
+  `expire_time` DATETIME NOT NULL DEFAULT '1000-01-01 00:00:00',
+  PRIMARY KEY (`token`),
+  FOREIGN KEY (`userid`) REFERENCES user(`userid`)
+);
+
+-- Table structure for table `diary`
+
+CREATE TABLE `diary` (
+  `diaryid` VARCHAR(16) NOT NULL,
+  `userid` VARCHAR(16) NOT NULL,
+  `emotion` SMALLINT NOT NULL,
+  `selfie` VARCHAR(128) NOT NULL,
+  `text` VARCHAR(2000) NOT NULL,
+  `create_time` DATETIME NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `edit_time` DATETIME NOT NULL DEFAULT '1000-01-01 00:00:00',
+  PRIMARY KEY (`diaryid`),
+  FOREIGN KEY (`userid`) REFERENCES user(`userid`)
+);
+
